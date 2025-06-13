3063fc47a6f8dd303653f5533f4a467c6e086843
Luna A <lunaalcantara.coding@gmail.com>
2025-06-12 20:49:52 -0500

    Initial Commit

---
+++ src/client/java/lunar/YipimodClient.java
@@ -0,0 +1,10 @@
+package lunar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YipimodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/lunar/ModItems.java
@@ -0,0 +1,33 @@
+package lunar;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.*;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Yipimod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void initialize() {
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.TBH_CREATURE));
+    }
+
+    public static final Item TBH_CREATURE = register("TBH_creature", Item::new, new Item.Settings());
+
+}
